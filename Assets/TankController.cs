0ef0cab1141e47a55b15bee0aabbd58d6c07a137
Thomas Fournier <thomas@artivain.com>
2023-04-22 17:59:58 -0400

    Try to start making basic controls

---
+++ Assets/TankController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TankController : MonoBehaviour
+{
+	public float speed = 10.0f;
+	public float rotationSpeed = 100.0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
